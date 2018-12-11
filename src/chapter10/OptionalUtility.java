18ca50c4c43cc8151f8d36b51a56526a3abaea18
Shiraz <shiraz.isam@jstack.eu>
2018-12-11 20:03:38 +0100

    exception changed

--- src/chapter10/OptionalUtility.java
+++ src/chapter10/OptionalUtility.java
@@ -9,7 +9,7 @@ public class OptionalUtility {
     public static Optional<Integer> stringToInt(String s) {
         try {
             return Optional.of(Integer.parseInt(s));
-        } catch (NumberFormatException e) {
+        } catch (RuntimeException e) {
             return Optional.empty();
         }
     }
