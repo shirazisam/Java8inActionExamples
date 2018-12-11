abff0ae21e563d9399330163438b7100d1cd47d5
Shiraz <shiraz.isam@jstack.eu>
2018-12-11 20:17:40 +0100

    comment re-aadded

--- src/patterns/factory/ProductFactory.java
+++ src/patterns/factory/ProductFactory.java
@@ -13,6 +13,7 @@ import static java.util.Map.Entry.comparingByValue;
  */
 public class ProductFactory {
 
+    /* A factory pattern using lambda - my_new_branch */
 
     final static Map<String, TriFunction<Double, String, Integer, Product>> map = new HashMap<>();
     static {
