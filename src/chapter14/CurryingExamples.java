0062534a3d0817fc7a08279d7f9bc12fc2eef4f5
Shiraz <shiraz.isam@jstack.eu>
2018-12-14 19:42:56 +0100

    More examples added

--- src/chapter14/CurryingExamples.java
+++ src/chapter14/CurryingExamples.java
@@ -1,6 +1,9 @@
 package chapter14;
 
+import java.util.Map;
 import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
 import java.util.stream.Stream;
 
 /**
@@ -13,11 +16,24 @@ public class CurryingExamples {
         return (double x) -> x * f + b;
     }
 
+    static Function<Integer, Integer> curryMutliply(int x) {
+        return (Integer y) -> x * y;
+    }
+
+
     public static void main(String[] args) {
 
         DoubleUnaryOperator cToFconverter = curriedConverter(9/5.0, 32);
 
         double farenheit = cToFconverter.applyAsDouble(100);  // C -> F
         System.out.println("farenheit = " + farenheit);
+
+        Function<Integer, Integer> curriedFunction = curryMutliply(12);
+        Integer result = curriedFunction.apply(3);
+        System.out.println("result = " + result);
+
+        Stream.of(1, 3, 5, 7)
+                .map(curryMutliply(2))
+                .forEach(System.out::println);
     }
 }
